b2519f439ec8fd5681cebbff14495e2b58ebe499
Pazhiloi <martinsen06@gmail.com>
2022-07-12 17:00:16 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import { BrowserRouter as Router, NavLink, Route } from "react-router-dom";
+import UsersPage from "./components/UsersPage";
+import TodosPage from "./components/TodosPage";
+import UserItemPage from "./components/UserItemPage";
+import TodoItemPage from "./components/TodoItemPage";
+const App: React.FC  = () =>  {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+    <Router>
+      <div>
+        <div>
+          <NavLink to="/users">Пользователи</NavLink>
+          <NavLink to="/todos">Список дел</NavLink>
+        </div>
+        <Route path={"/users"} exact>
+          <UsersPage />
+        </Route>
+        <Route path={"/todos"} exact>
+          <TodosPage />
+        </Route>
+        <Route path={"/users/:id"}>
+          <UserItemPage />
+        </Route>
+        <Route path={"/todos/:id"}>
+          <TodoItemPage />
+        </Route>
+      </div>
+    </Router>
     </div>
   );
 }
